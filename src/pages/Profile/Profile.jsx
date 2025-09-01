ad01d32ffab61786bcf032b8d7982524745fedee
mandoromero <mandoromero1976@yahoo.com>
2025-08-31 17:41:55 -0700

    "commit"

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,15 +1,18 @@
+import "./Profile.css";
+
 export default function Profile() {
   return (
     <div className="profile">
-      <h2>User Profile</h2>
-      <p>Welcome to your profile page!</p>
-      <p>Here you can view and edit your profile information.</p>
-      <div>
-        <h3>Your Information</h3>
-        <p>Name: John Doe</p><span><p>UserID:</p></span>
-        <p>Email: john.doe@example.com</p>
-        <p>Birthdate: 2000-01-01</p>
-      </div>
+        <div className="profile-container">
+            <h2 clasaName="profile-title">User Profile</h2>
+            <p className="profile-greeting">Welcome to your profile page!</p>
+            <p className="profile-greeting">Here you can view and edit your profile information.</p>
+            <h3 className="profile-subtitle">Your Information</h3>
+            <p className="user-info">Name: John Doe</p>
+            <p className="user-info">UserID:</p>
+            <p className="user-info">Email: john.doe@example.com</p>
+            <p>Birthdate: 2000-01-01</p>
+        </div>
     </div>
   );
 }
